e5dd3a849d9a0f32aea853f4c8e925b1bda858df
Maxim <maximgratii@gmail.com>
2024-02-22 13:26:10 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# test-react
+# @feedefy/react
+
+The [Feedefy](https://www.feedefy.com/) widget for your React app.
+
+## Usage
+
+See [the documentation](https://app.feedefy.com/instructions)
+
+## License
+
+Licensed under the MIT license.
---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import { useEffect } from "react";
+
+export const Feedefy = ({ projectId }: { projectId: string }) => {
+  useEffect(() => {
+    if (!projectId) {
+      console.error("No projectId was passed to Feedefy React, widget will not be initialized");
+      return;
+    }
+
+    const script = document.createElement("script");
+
+    script.src = `https://app.feedefy.com/widget.js?id=${projectId}`;
+
+    script.defer = true;
+
+    script.addEventListener("error", () => script.remove());
+
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, [projectId]);
+
+  return null;
+};
