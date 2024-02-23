4e402615f0f14effcfb930b1c08d9d8c717f9518
Maxim <maximgratii@gmail.com>
2024-02-23 17:32:27 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ export const Feedefy = ({ projectId }: { projectId: string }) => {
       return;
     }
 
+    // @ts-ignore
+    if (window.feedefyInjected) {
+      return;
+    }
+
+    // @ts-ignore
+    window.feedefyInjected = true;
+
     const script = document.createElement("script");
 
     script.src = `https://app.feedefy.com/embed.js?id=${projectId}`;
-
     script.defer = true;
-
     script.addEventListener("error", () => script.remove());
 
     document.head.appendChild(script);
