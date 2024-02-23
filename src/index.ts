bb6be093c914381e69a7dc9ff06062cf0a015829
Maxim <maximgratii@gmail.com>
2024-02-23 18:22:23 +0200

    fix

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,18 @@ export const Feedefy = ({ projectId }: { projectId: string }) => {
       return;
     }
 
-    // @ts-ignore
-    if (window.feedefyInjected) {
+    const existingScript = document.querySelector(`script[src*="https://app.feedefy.com"]`);
+
+    if (existingScript) {
       return;
     }
-    // @ts-ignore
-    window.feedefyInjected = true;
+
+    // // @ts-ignore
+    // if (window.feedefyInjected) {
+    //   return;
+    // }
+    // // @ts-ignore
+    // window.feedefyInjected = true;
 
     const script = document.createElement("script");
 
@@ -21,6 +27,21 @@ export const Feedefy = ({ projectId }: { projectId: string }) => {
     script.addEventListener("error", () => script.remove());
 
     document.head.appendChild(script);
+
+    return () => {
+      // const button = document.querySelector(".feedefy-floating-button");
+      // const widget = document.querySelector("iframe");
+      // console.log(widget);
+      // const widget2 = widget.querySelector(".feedefy-widget");
+      // console.log(widget2);
+      // if (widget) {
+      //   widget.remove();
+      // }
+      // if (button) {
+      //   button.remove();
+      // }
+      // script.remove();
+    };
   }, [projectId]);
 
   return null;
